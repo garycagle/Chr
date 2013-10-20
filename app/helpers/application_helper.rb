36e6043a6598910f43805ec1ebd4f7bac7493b2e
Gary Cagle <gcagle@bitstream.net>
2013-10-19 22:43:07 -0500

    prior to bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.       # Documentation comment
   def full_title(page_title)                          # Method definition
-    base_title = "Chro_nose Sample App"  # Variable assignment
+    base_title = "Chro_nose "  # Variable assignment
     if page_title.empty?                              # Boolean test
       base_title                                      # Implicit return
     else
